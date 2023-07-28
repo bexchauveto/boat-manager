7443b6bec8d4f6fcdca548b159fe2e1e99c35702
Olivier Bex-Chauvet <olivier@bex-chauvet.fr>
2023-07-28 20:01:41 +0800

    [Fix] the configuration for the server in swagger

--- src/main/java/com/bexchauvet/boatmanager/config/OpenAPIConfig.java
+++ src/main/java/com/bexchauvet/boatmanager/config/OpenAPIConfig.java
@@ -3,24 +3,28 @@ package com.bexchauvet.boatmanager.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(title = "Boat API", version = "1.0",
                 contact = @Contact(name = "Olivier Bex-Chauvet", email = "olivier@bex-chauvet.fr",
                         url = "https://bew-chauvet.fr"),
-                description = "This API is created to manage a fleet of boat all around the world"),
-        servers = {
-                @Server(url = "http://localhost:8080", description = "Development"),
-                @Server(url = "https://bex-chauvet.fr/", description = "Production")})
+                description = "This API is created to manage a fleet of boat all around the world"))
 public class OpenAPIConfig {
+
+
+    @Value("${server-swagger}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI customizeOpenAPI() {
@@ -35,7 +39,8 @@ public class OpenAPIConfig {
                                 .scheme("bearer")
                                 .description(
                                         "Provide the JWT token. JWT token can be obtained from the Login API.")
-                                .bearerFormat("JWT")));
+                                .bearerFormat("JWT"))).servers(List.of(new Server().url(serverUrl)
+                        .description(serverUrl.contains("localhost") ? "Development" : "Production")));
 
     }
 
